277cf6396dbf654aab63345425e19a48ed2417d1
Murillo <murillom.martinho@gmail.com>
2021-02-15 10:45:23 -0300

    Implementação do Edit do Seller

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMVC.Services.Exceptions;
 
 namespace SalesWebMVC.Services
 {
@@ -38,6 +39,22 @@ namespace SalesWebMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(sel => sel.Id == obj.Id))
+            {
+                throw new NotFoundException("ID not found");
+            }
+
+            try { 
+                _context.Update(obj);
+                _context.SaveChanges();
+            } catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
