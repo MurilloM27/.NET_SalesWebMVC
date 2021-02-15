81ac9d2bfdf489fb13b5df96381901157cfd7549
Murillo <murillom.martinho@gmail.com>
2021-02-15 12:25:00 -0300

    Validações dos formulários

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -9,18 +9,31 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "{0} should be between {2} and {1}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid e-mail")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
         [Display(Name = "Base Salary")]
+        [Range(100.0, 10000.0, ErrorMessage = "{0} should be between {1} and {2}")]
         [DataType(DataType.Currency)]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
+
         [Display(Name = "Department")]
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
        
         public Seller() { }
